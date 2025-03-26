97bf7129cdc307482f29ce270eab6665ac46d768
sunshinestarxo <123594675+sunshinestarxo@users.noreply.github.com>
2025-03-26 13:59:59 +0100

    changed app.py

--- app.py
+++ app.py
@@ -6,7 +6,22 @@ plotter = PlotGenerator()
 def show_plot():
     return plotter.create_plot()
 
-# Interface definieren
-demo = gr.Interface(fn=show_plot, inputs=[], outputs=gr.Plot(label="Interaktiver Formel 1 Plot"))
+with gr.Blocks() as demo:
+    gr.HTML("""
+        <div style="display: flex; flex-direction: column; align-items: center; font-family: 'Courier New', sans-serif;">
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmXwvdFYsHObbXaxFauMwKO-k3MjcWVzx5kT4jMh3eOlV9hFhsijdinE93sX9dCSGvCKw&usqp=CAU' 
+                 alt='Formel 1 Logo' style='width:200px; margin-bottom: 10px;'/>
+            <h1 style="color: #FF1801; font-family: 'Orbitron', sans-serif; font-size: 64 px; margin: 0;"> Formel 1 Dashboard</h1>
+            <p style="font-size: 18px; margin-top: 5px;">Hier findest du aktuelle Daten zur Saison 2025.</p>
+        </div>
+    """)
+
+
+
+    with gr.Row():
+        plot_output = gr.Plot(label="Interaktiver Formel 1 Plot")
+        plot_button = gr.Button("Zeige Plot")
+
+    plot_button.click(fn=show_plot, inputs=[], outputs=plot_output)
 
 demo.launch()
--- plots.py
+++ plots.py
@@ -1,7 +1,21 @@
 # plots.py
 import plotly.graph_objects as go
+import pandas as pd 
+
 
 class PlotGenerator:
+    def __init__(self):
+        self.result = pd.read_csv('f1db_csv/results.csv')
+        self.drivers = pd.read_csv('f1db_csv/drivers.csv')
+        self.races = pd.read_csv('f1db_csv/races.csv')
+
+
+    def plot_winner(self):
+        pass
+
+
+
+
     def create_plot(self):
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=[1, 2, 3], y=[1, 4, 9]))
